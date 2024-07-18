92dcff3c3af0d25ce0300697809d55a9cdd4e7e9
Filipe Cesar <filipeccfds@outlook.com>
2024-07-18 19:44:30 -0300

    dataframe e filtros

--- day02/import_csv.py
+++ day02/import_csv.py
@@ -20,6 +20,53 @@ df_customers["Points"].describe()
 
 # %%
 
+df_customers["Points"] + 1000
+
+# %%
+
+condicao = df_customers["Points"] > 1000
+df_customers[condicao]
+
+# %%
+
+condicao = df_customers["Points"] == df_customers["Points"].max()
+df_customers[condicao]
+
+# %%
+condicao = df_customers["Points"] == df_customers["Points"].max()
+df_maior = df_customers[condicao]
+df_maior["Name"].iloc[0]
+
+
+# %%
+
+df_customers[["UUID", "Name"]]
+# %%
+
+df_colums = df_customers.columns.to_list()
+df_colums.sort()
+# cria um novo post nao atribuido ao prinicipal
+#este atribui a dataframe principal
+df_customers = df_customers[df_colums]
+
+# %%
+df_customers.rename(columns={"Name":"Nome",
+                             "Points":"Pontos"
+                })
+# %%
+
+df_customers.rename(columns={"UUID":"ID" }, inplace=True)
+df_customers
+
+
+
+# %%
+condicao = (df_customers["Points"] >= 1000) & (df_customers["Points" >= 2000])
+df_1000_2000 = df_customers[condicao]
+
+
+
+
 notas = [ 34,43 ,23 ,1 ,5 ,7 ]
 for i in notas:
     if i >=7:
